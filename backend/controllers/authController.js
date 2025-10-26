5ec2699bc7004c23800d346f024296875da9e971
Nikhil <nikhilnnair02@gmail.com>
2025-10-26 23:20:06 +0530

    feat: implemented backend email verification with OTP during signup; add nodemailer dependency

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
 import validator from 'validator';
+import nodemailer from "nodemailer";
 
 export const signup = async (req, res) => {
   try {
@@ -17,10 +18,55 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    user = new User({ name, email, password: hashedPassword });
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    user = new User({ name, email, password: hashedPassword , otp, otpExpires: Date.now() + 10 * 60 * 1000 }); // OTP valid for 10 mins
     await user.save();
 
-    res.status(201).json({ message: "User registered successfully" });
+    // Send OTP email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // your Gmail
+        pass: process.env.EMAIL_PASS, // your app password
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Verify your email - ToothAid",
+      text: `Your OTP is ${otp}. It is valid for 10 minutes.`,
+    };
+    await transporter.sendMail(mailOptions);
+
+    res.status(201).json({ message: "OTP sent successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const verifyOTP = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ msg: "User not found" });
+
+    if (user.verified)
+      return res.status(400).json({ msg: "User already verified" });
+
+    if (user.otp !== otp)
+      return res.status(400).json({ msg: "Invalid OTP" });
+
+    if (user.otpExpires < new Date())
+      return res.status(400).json({ msg: "OTP expired" });
+
+    user.verified = true;
+    user.otp = undefined;
+    user.otpExpires = undefined;
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully!" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -38,6 +84,10 @@ export const login = async (req, res) => {
     // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    // Check if verified
+    // if (!user.verified)
+    //   return res.status(400).json({ msg: "Email not verified" });
 
     // Generate token
     const token = jwt.sign({ id: user._id,name:user.name }, process.env.JWT_SECRET, { expiresIn: "1d" });
